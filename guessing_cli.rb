58bb86176a532f49f8a1e0215f868f9de890a647
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-21 19:11:56 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,6 +11,8 @@ def run_guessing_game
       puts "You guessed the correct number!"
     elsif guess.to_i != comp_rand
       puts "The computer guessed #{comp_rand}"
+    else
+      puts "Invalid input"
 
   end
 end
